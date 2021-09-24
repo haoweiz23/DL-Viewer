82ce268e1de2cfa9d035a307830f895220b38bff
Haowei Zhu <haoweiz@xlnx.xilinx.com>
2021-09-24 12:41:58 +0800

    add heatmap edit ui

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
 from PyQt5.Qt import *
+from PyQt5.QtCore import *
 import sys
 import os
 import matplotlib.pyplot as plt
 from ui import Ui_MainWindow
 from qt_material import apply_stylesheet
+# from utils import gradcam
+
+
+class ModelSetting_Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Model Setting")
+        Dialog.resize(400, 300)
+        self.pushButton = QPushButton(Dialog)
+        self.pushButton.setGeometry(QRect(160, 100, 75, 23))
+        self.pushButton.setObjectName("pushButton")
+        Dialog.setWindowFlags(Qt.WindowStaysOnTopHint)  # 设置窗体总显示在最上面
+        self.retranslateUi(Dialog)
+        QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.pushButton.setText(_translate("Dialog", "PushButton"))
+
+
+class ModelSetting(QDialog):
+    def __init__(self):
+        QDialog.__init__(self)
+        self.child = ModelSetting_Ui_Dialog()
+        self.child.setupUi(self)
 
 
 class MyMainWindow(QMainWindow, Ui_MainWindow):
@@ -12,26 +38,42 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
         super(MyMainWindow, self).__init__(parent)
         self.setupUi(self)
         self.actionView.triggered.connect(self.call_back_action_actionView)
-        # self.actionView.setIcon(QIcon('icons/image_view.png'))
         self.actionDraw_Fig.triggered.connect(self.call_back_action_actionDraw_Fig)
-        # self.actionDraw_Fig.setIcon(QIcon('icons/figure_paint.png'))
+        self.actionEdit.triggered.connect(self.call_back_action_actionEdit)
+        self.page3_open_dir.setIcon(QIcon("./icons/open_dir.png"))
+        self.page3_open_file.setIcon(QIcon("./icons/open-file.png"))
+        self.page3_build.setIcon(QIcon("./icons/build.png"))
+        self.page3_save.setIcon(QIcon("./icons/save.png"))
+        self.page3_clear.setIcon(QIcon("./icons/clear.png"))
+
         self.pushButton_load_table.clicked.connect(self.call_back_push_button_load_table)
         self.pushButton_build.clicked.connect(self.call_back_push_button_build)
+        self.page3_open_file.clicked.connect(self.call_back_page3_button_open_file)
+        self.page3_img_list_widget.currentRowChanged.connect(self.call_back_page3_img_list_widget_clicked)
+        self.page3_img_list_widget.clicked.connect(self.call_back_page3_img_list_widget_clicked)
+
+        self.message = message(self)
+        self.message.signal.connect(self.heatmap_param_dialog)
+        self.dialog = childWindow()
+        self.page3_heatmap.clicked.connect(self.dialog.show)
 
         self.H_Max, self.W_Max = 960, 960
-        # self.additem('./imgs/0181723.jpg', self.page1_imglistWidget)
-        # self.page2_x_column.setText('1')
         self.page1_imglistWidget.dropEvent = self.dropEvent
         self.page1_imglistWidget.dragEnterEvent = self.dragEnterEvent
-        self.page1_imglistWidget.setStyleSheet('border-width: 1px;border-style: solid;border-color: rgb(125, 125, 125);')
-        self.page1_result_listwidget.setStyleSheet('border-width: 1px;border-style: solid;border-color: rgb(125, 125, 125);')
+        self.page1_imglistWidget.setStyleSheet(
+            'border-width: 1px;background: white;border-style: solid;border-color: rgb(125, 125, 125);')
+        self.page1_result_listwidget.setStyleSheet(
+            'border-width: 1px;background: white;border-style: solid;border-color: rgb(125, 125, 125);')
         self.page1_text_outputs.setStyleSheet('border-width: 1px;border-style: solid;border-color: rgb(125, 125, 125);')
 
         # self.setMouseTracking(True)
-        self.mousePosX,  self.mousePosY = 120, 38
-        self.mousePosX_gap,  self.mousePosY_gap = 0, 0
-        self.mousePosX_Flag,  self.mousePosY_Flag = False, False
+        self.mousePosX, self.mousePosY = 120, 38
+        self.mousePosX_gap, self.mousePosY_gap = 0, 0
+        self.mousePosX_Flag, self.mousePosY_Flag = False, False
 
+    def heatmap_param_dialog(self):
+        # QMessageBox.information(self, 'Warning', 'Success', QMessageBox.Ok)
+        Ui_Dialog().show()
 
         # self.toolBar.setStyleSheet("QToolBar{spacing:8px;}")
         # self.actionView.icon.setStyleSheet("QToolBar{spacing:8px;}")
@@ -67,6 +109,9 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
 
     def call_back_action_actionDraw_Fig(self):
         self.stackedWidget.setCurrentIndex(1)
+
+    def call_back_action_actionEdit(self):
+        self.stackedWidget.setCurrentIndex(2)
 
     def call_back_push_button_load_table(self):
         file, filetype = QFileDialog.getOpenFileName(self, "Select file", "",
@@ -125,12 +170,42 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
         except Exception as e:
             print(e)
 
-    def additem(self, img_path, obj):
+    # def call_back_page3_heatmap(self):
+    #     try:
+    #
+    #     except Exception as e:
+    #         print(e)
+
+    def call_back_page3_button_open_file(self):
+        files, filetype = QFileDialog.getOpenFileNames(self, "Select files", ".",
+                                                       "All Files (*);;Img Files (*.png, *.jpg, *.jpeg)")
+        try:
+            for file in files:
+                if file.endswith('.jpg') or file.endswith('.png'):
+                    self.additem(file, self.page3_img_list_widget)
+        except:
+            pass
+
+    def additem(self, img_path, obj, set_text=True):
         pix1 = QPixmap(img_path).scaled(self.page1_imglistWidget.iconSize())
         item = QListWidgetItem()
         item.setIcon(QIcon(pix1.scaled(self.H_Max, self.W_Max, Qt.KeepAspectRatio, Qt.SmoothTransformation)))
-        # item.setCheckState(Qt.CheckState.Unchecked)
+        if set_text:
+            item.setToolTip(img_path)
+            # item.setText(os.path.split(img_path)[-1])
         obj.addItem(item)
+
+    def call_back_page3_img_list_widget_clicked(self, event):  # set background_img
+        try:
+            items = self.page3_img_list_widget.selectedItems()
+            for item in items:
+                h, w = self.page3_target_img.height(), self.page3_target_img.width()
+                img = QPixmap(item.toolTip()).scaled(w, h, Qt.KeepAspectRatio, Qt.SmoothTransformation)
+                self.page3_target_img.setPixmap(img)
+
+                self.page3_result_img.setPixmap(img)
+        except Exception as e:
+            print(e)
 
     def keyReleaseEvent(self, e: QKeyEvent):  # sometimes keyPressEvent not work, switch to keyReleaseEvent
         try:
@@ -245,9 +320,9 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
             self.page1_imglistWidget.resize(self.page1_imglistWidget.geometry().width() + xChanged,
                                             self.page1_imglistWidget.geometry().height())
             self.page1_result_listwidget.setGeometry(self.page1_result_listwidget.geometry().x() + xChanged,
-                                                     self.page1_result_listwidget.geometry().y() ,
+                                                     self.page1_result_listwidget.geometry().y(),
                                                      self.page1_result_listwidget.geometry().width() - xChanged,
-                                            self.page1_result_listwidget.geometry().height())
+                                                     self.page1_result_listwidget.geometry().height())
             self.mousePosX = event.x()
         if self.mousePosY_Flag:
             yChanged = event.y() - self.mousePosY
@@ -258,10 +333,21 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
                                                      self.page1_result_listwidget.geometry().width(),
                                                      self.page1_result_listwidget.geometry().height() + yChanged)
             self.page1_text_outputs.setGeometry(self.page1_text_outputs.geometry().x(),
-                                                     self.page1_text_outputs.geometry().y() + yChanged,
-                                                     self.page1_text_outputs.geometry().width(),
-                                                     self.page1_text_outputs.geometry().height() - yChanged)
+                                                self.page1_text_outputs.geometry().y() + yChanged,
+                                                self.page1_text_outputs.geometry().width(),
+                                                self.page1_text_outputs.geometry().height() - yChanged)
             self.mousePosY = event.y()
+
+
+class message(QThread):
+    signal = pyqtSignal()
+
+    def __init__(self, Window):
+        super(message, self).__init__()
+        self.window = Window
+
+    def run(self):
+        self.signal.emit()
 
 
 if __name__ == "__main__":
